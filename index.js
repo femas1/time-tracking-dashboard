936b4eb87fb4c8b9dc2dfde80103b425675ac718
femas1 <federico.massara2@gmail.com>
2022-11-25 11:37:22 +0100

    add js file get cards

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const cardWorkWeekly = document.querySelector('.card__work__hours--weekly');
+const cardPlayWeekly = document.querySelector('.card__play__hours--weekly');
+const cardStudyWeekly = document.querySelector('.card__study__hours--weekly');
+const cardExerciseWeekly = document.querySelector('.card__exercise__hours--weekly');
+const cardSocialWeekly = document.querySelector('.card__social__hours--weekly');
+const cardSelfcareWeekly = document.querySelector('.card__selfcare__hours--weekly');
+

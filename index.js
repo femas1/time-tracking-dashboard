df15e209d6bc0d25cbc48dff2de7c5147006af07
femas1 <federico.massara2@gmail.com>
2022-11-25 12:30:52 +0100

    add js logic adding times to cards

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
-fetch('./data.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+const getHourData = fetch('./data.json')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        cardWorkWeekly.innerHTML = `${data[0].timeframes.monthly.current}hrs
+        `
+    })
+
+
+const cardWorkDaily = document.querySelector('.card__work__hours--daily');
+const cardPlayDaily = document.querySelector('.card__play__hours--daily');
+const cardStudyDaily = document.querySelector('.card__study__hours--daily');
+const cardExerciseDaily = document.querySelector('.card__exercise__hours--daily');
+const cardSocialDaily = document.querySelector('.card__social__hours--daily');
+const cardSelfcareDaily = document.querySelector('.card__selfcare__hours--daily');
 
 const cardWorkWeekly = document.querySelector('.card__work__hours--weekly');
 const cardPlayWeekly = document.querySelector('.card__play__hours--weekly');
@@ -12,3 +23,9 @@ const cardExerciseWeekly = document.querySelector('.card__exercise__hours--weekl
 const cardSocialWeekly = document.querySelector('.card__social__hours--weekly');
 const cardSelfcareWeekly = document.querySelector('.card__selfcare__hours--weekly');
 
+const cardWorkMonthly = document.querySelector('.card__work__hours--monthly');
+const cardPlayMonthly = document.querySelector('.card__play__hours--monthly');
+const cardStudyMonthly = document.querySelector('.card__study__hours--monthly');
+const cardExerciseMonthly = document.querySelector('.card__exercise__hours--monthly');
+const cardSocialMonthly = document.querySelector('.card__social__hours--monthly');
+const cardSelfcareMonthly = document.querySelector('.card__selfcare__hours--monthly');
